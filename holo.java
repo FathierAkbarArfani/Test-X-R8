935828bd2eab2a573abc8d245f2a666978460976
FathierAkbarArfani <fathier_arfani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:10:53 +0700

    Add files via upload

---
+++ holo.java
@@ -0,0 +1,21 @@
+public class holo {
+     public static void main(String [] args) {
+          System.out.println("This is Moral Minus, i don't care who you are, i don't care what you want. But i will find you and i will kill you");
+
+          String codename="Moral_Minus";
+          String Location="Javanese";
+          int Age=15;
+          double beratBadan=45.00;
+          float tinggiBadan=159f;
+          char jenisKelamin='P'; 
+          boolean status = false;
+
+          System.out.println("Codename: "+codename);
+          System.out.println("Age: "+Age);
+          System.out.println("Location: "+Location);
+          System.out.println("Tinggi Badan: "+tinggiBadan);
+          System.out.println("Jenis Kelamin: "+jenisKelamin);
+          System.out.println("Status: "+status);
+   }
+ 
+ }
